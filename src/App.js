b15a322af0fd33102c9387c5b6ce1e1d683062a1
Malcolm Cumming <bemjogar@gmail.com>
2017-09-04 20:35:53 +0100

    sass structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
-import logo from './logo.svg';
-import Header from './components/Header.js';
+import Header from './components/Header/Header.js';
 import NumPlayers from './screens/NumPlayers.js';
 import EnterName from './screens/EnterName.js';
 import notFound from './screens/notFound.js';
-import './App.css';
 
 class App extends React.Component {
   render() {
