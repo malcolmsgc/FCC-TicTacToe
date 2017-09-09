804fa9706cad03f0f815e975e2d317e797409df2
Malcolm Cumming <bemjogar@gmail.com>
2017-09-09 22:13:04 +0100

    reverse logic for toggle visibility

--- src/components/ShareLinkBlock/ShareLinkBlock.js
+++ src/components/ShareLinkBlock/ShareLinkBlock.js
@@ -4,12 +4,12 @@ import './sharelinkblock.css';
 class ShareLinkBlock extends React.Component {
 
 componentDidMount() {
-    if (!this.props.showBlock) this.p2Link.classList.add('hidden');
+    if (this.props.showBlock) this.p2Link.classList.remove('hidden');
 }
 
 render() {
     return (
-        <div className="msgblock-P2link" ref={ (node) => {this.p2Link = node}}>
+        <div className="msgblock-P2link hidden" ref={ (node) => {this.p2Link = node}}>
             <div className="wrapper">
                 <p>If player 2 is using another device, send them this link:</p>
                 <div id="copy-P2-link">
