93885ad562bcfec8d8d2f43503957e593d618911
Malcolm Cumming <bemjogar@gmail.com>
2017-09-12 22:26:37 +0100

    StatsBar working with CSS vars

--- src/components/StatsBar/StatsBar.js
+++ src/components/StatsBar/StatsBar.js
@@ -4,15 +4,32 @@ import './statsbar.css';
 
 class StatsBar extends React.Component {
 
+    constructor(){
+        super();
+        this.updateStats = this.updateStats.bind(this);
+    }
+
+updateStats() {
+    const gamesPlayed = this.props.gamesPlayed;
+    if (gamesPlayed > 0) {
+        const p1wins = this.props.player1.won;
+        const p2wins = this.props.player2.won;
+        const draws = gamesPlayed - (p1wins + p2wins);
+        console.log(`Draws: ${draws / gamesPlayed * 100}%`);
+        console.log(`P1: ${p1wins / gamesPlayed * 100}%`);
+        console.log(`P2: ${p2wins / gamesPlayed * 100}%`);
+        this.drawStats.style.setProperty("--draws", `${draws / gamesPlayed * 100}%`);
+        this.p1Stats.style.setProperty("--p1wins", `${p1wins / gamesPlayed * 100}%`);
+        this.p2Stats.style.setProperty("--p2wins", `${p2wins / gamesPlayed * 100}%`);
+    }
+}
+
 render() {
-    const p1wins = this.props.player1.won;
-    const p2wins = this.props.player2.won;
-    const draws = this.props.gamesPlayed - (p1wins + p2wins);
     return (
         <div className="stats-bar">
-            <span className="p1wins">{p1wins}</span>
-            <span className="draws">{draws}</span> 
-            <span className="p2wins">{p2wins}</span>
+            <span className="p1wins" ref={ (node) => {this.p1Stats = node} }></span>
+            <span className="draws" ref={ (node) => {this.drawStats = node} }></span> 
+            <span className="p2wins" ref={ (node) => {this.p2Stats = node} }></span>
         </div>
     );
 }
