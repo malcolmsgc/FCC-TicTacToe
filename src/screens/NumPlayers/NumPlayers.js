8c870121f62e787abca8f3a1e3904b59c8056c50
Malcolm Cumming <bemjogar@gmail.com>
2017-10-06 20:56:24 +0100

    Misc styling

--- src/screens/NumPlayers/NumPlayers.js
+++ src/screens/NumPlayers/NumPlayers.js
@@ -18,7 +18,7 @@ class NumPlayers extends React.Component {
     render() {
         return (
     
-                <div>
+                <div className="num-players">
                     <MessageBlock messageText="How many players?"/>
                         <BaseButton buttonType="button" buttonText="1 Player" className="p1" btnAction={() => {this.props.isTwoPlayer(false);this.next();} }/>
                         <BaseButton buttonType="button" buttonText="2 Players" className="p2" btnAction={() => {this.props.isTwoPlayer(true);this.next();} }/>
